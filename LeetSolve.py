95e9be1c4ce9555b99993682b5161b3044e6ae81
Sid Lalgowdar <sidlalgowdar@Sids-Air.eduroam-wless.du.edu>
2025-03-06 21:23:39 -0700

    qwen2.5:7b

--- LeetSolve.py
+++ LeetSolve.py
@@ -4,22 +4,24 @@ import warnings
 from PIL import Image
 import pytesseract
 import pyautogui
-import time
 import pyperclip
+import time
 
 warnings.filterwarnings("ignore", message="cumsum_out_mps supported by MPS on MacOS 13+")
 
-print("Open your leetcode page within 5 seconds. Answer will be generated soon.")
-time.sleep(5)
-screenshot = pyautogui.screenshot(region=(15, 310, 695, 1000))
-screenshot.save("q.png")
+time.sleep(3)
 
-screenshot2 = pyautogui.screenshot(region=(785, 250, 700, 250))
-screenshot2.save("sn.png")
+def screenshots():
+    screenshot = pyautogui.screenshot(region=(15, 310, 695, 1000))
+    screenshot.save("q.png")
+
+    screenshot2 = pyautogui.screenshot(region=(785, 250, 700, 250))
+    screenshot2.save("sn.png")
 
 def extract_text_from_image(image, snip):
     return pytesseract.image_to_string(image), pytesseract.image_to_string(snip)
 
+screenshots()
 question, snipp = extract_text_from_image(Image.open("q.png"), Image.open("sn.png"))
 
 question = question.split("Example 1")[0]
@@ -27,16 +29,13 @@ question = question.split("Example 1")[0]
 print(question+"\n")
 print(snipp+"\n")
 
-model = OllamaLLM(model="llama3.1:8b")
+model = OllamaLLM(model="qwen2.5:7b")
 
 template = """Solve the incomplete snippet in the question in Python 3.
 If a module offers a speed boost, always import and use it.
 Make sure they are modules included with Python.
-Class name must be Solution.
 Don't add a main to call the class or run the program.
-Basically code it up like a LeetCode problem.
 Make sure the program runs as fast as possible.
-If not make the code as small and/or elegant as possible.
 
 Question: {question}
 
@@ -58,8 +57,5 @@ ans = chain.invoke({"question": question, "snipp": snipp})
 print(ans)
 print("Snippet generated.")
 
-user = input("Copy snippet? ")
-if user.lower() == "y":
-    pyperclip.copy(ans)
-    print("Copied to clipboard")
-
+pyperclip.copy(ans)
+print("Copied to clipboard")
