bab3a9a332445fd574e3a40a7bd8a4aa6b79d67b
Sid Lalgowdar <sidlalgowdar@Sids-Air.eduroam-wless.du.edu>
2025-03-06 22:08:12 -0700

    added some visual indicator, needs to be improved ofc

--- LeetSolve.py
+++ LeetSolve.py
@@ -5,11 +5,12 @@ from PIL import Image
 import pytesseract
 import pyautogui
 import pyperclip
-import time
+import pygame, sys
+
+screen = pygame.display.set_mode((500, 20))
 
 warnings.filterwarnings("ignore", message="cumsum_out_mps supported by MPS on MacOS 13+")
-
-time.sleep(3)
+model = OllamaLLM(model="qwen2.5:7b")
 
 def screenshots():
     screenshot = pyautogui.screenshot(region=(15, 310, 695, 1000))
@@ -21,19 +22,18 @@ def screenshots():
 def extract_text_from_image(image, snip):
     return pytesseract.image_to_string(image), pytesseract.image_to_string(snip)
 
-screenshots()
-question, snipp = extract_text_from_image(Image.open("q.png"), Image.open("sn.png"))
+def get_solution(model):
+    screenshots()
+    question, snipp = extract_text_from_image(Image.open("q.png"), Image.open("sn.png"))
 
-question = question.split("Example 1")[0]
+    question = question.split("Example 1")[0]
 
-print(question+"\n")
-print(snipp+"\n")
+    print(question+"\n")
+    print(snipp+"\n")
 
-model = OllamaLLM(model="qwen2.5:7b")
-
-template = """Solve the incomplete snippet in the question in Python 3.
+    template = """Solve the incomplete snippet in the question in Python 3.
 If a module offers a speed boost, always import and use it.
-Make sure they are modules included with Python.
+Make sure they are modules included with Python 3 and doesn't require the user to use pip install.
 Don't add a main to call the class or run the program.
 Make sure the program runs as fast as possible.
 Try to be as memory efficient as possible.
@@ -47,20 +47,43 @@ Handle edge cases as well.
 Be precise.
 """ 
 
-prompt = ChatPromptTemplate.from_template(template)
+    prompt = ChatPromptTemplate.from_template(template)
 
-chain = prompt | model
+    chain = prompt | model
 
-print("Generating response...\n")
+    print("Generating response...\n")
 
-ans = chain.invoke({"question": question, "snipp": snipp})
+    ans = chain.invoke({"question": question, "snipp": snipp})
 
-print(ans)
-print("Snippet generated.")
+    print(ans)
+    print("Snippet generated.")
 
-pyperclip.copy(ans)
-print("Copied to clipboard")
+    pyperclip.copy(ans)
+    print("Copied to clipboard")
 
-pyautogui.click()
-pyautogui.hotkey("command", "a")
-pyautogui.hotkey("command", "v")
+status = (0,0,0)
+clock = pygame.time.Clock()
+
+while True:
+    screen.fill(status)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+
+        elif event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_o) and (event.mod & pygame.KMOD_CTRL):
+                if status == (0,0,0):
+                    status = (255,255,0)
+                    screen.fill(status)
+                    pygame.display.update()
+                    get_solution(model)
+                    status = (0,255,0)
+            elif (event.key == pygame.K_n) and (event.mod & pygame.KMOD_CTRL):
+                if status == (0,255,0):
+                    status = (0,0,0)
+            elif (event.key == pygame.K_q) and (event.mod & pygame.KMOD_CTRL):
+                pygame.quit()
+                sys.exit(0)
+    pygame.display.update()
+    clock.tick(15)
